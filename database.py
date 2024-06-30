21643591510d31a59ef77d41f3b39f070f86f605
Meenakshi242 <meenuraguraman@gmail.com>
2024-06-30 17:02:25 +0530

    database.py

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
+CREATE DATABASE project_2;
+
 USE project_2;
 
 CREATE TABLE movies (
     id INT AUTO_INCREMENT PRIMARY KEY,
     movies VARCHAR(255),
     hall VARCHAR(255),
-    date varchar(255),
-    time varchar(255),
+    date VARCHAR(255),
+    time VARCHAR(255),
     charges int
 );
 
@@ -14,14 +16,14 @@ CREATE TABLE Booking_Details (
     name VARCHAR(255),
     movies VARCHAR(255),
     hall VARCHAR(255),
-    time varchar(255),
-    date varchar(255),
+    time VARCHAR(255),
+    date VARCHAR(255),
     charges_with_gst int,
     seat_num VARCHAR(255)
 );
 
 INSERT INTO movies (movies, hall, date, time, charges)
-values ('karudan', 'A', '24 june', '12.30pm', 250),
+VALUES ('karudan', 'A', '24 june', '12.30pm', 250),
        ('rathnam', 'C', '24 june', '10.00pm', 200),
        ('star', 'B', '25 june', '4.00pm', 190),
        ('j baby', 'B', '26 june', '2.30pm', 250),
